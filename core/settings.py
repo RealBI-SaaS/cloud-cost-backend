45497d3b2bf68c0814e325af9ba2010aa135065e
Nathnael <nathnaelyirga@gmail.com>
2025-03-20 13:59:22 +0300

    pythonanywhere deployment fixes and setup

--- core/settings.py
+++ core/settings.py
@@ -85,23 +85,26 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("PG_DB_NAME"),
-        "USER": env("PG_USER"),
-        "PASSWORD": env("PG_PASSWORD"),
-        "HOST": env("PG_HOST", "localhost"),
-        "PORT": env("PG_PORT", "5432"),
-        "CONN_MAX_AGE": None,
-        "OPTIONS": {"sslmode": env("PG_SSL_MODE")},
-    },
-    "sqlite": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-}
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": env("PG_DB_NAME"),
+            "USER": env("PG_USER"),
+            "PASSWORD": env("PG_PASSWORD"),
+            "HOST": env("PG_HOST", "localhost"),
+            "PORT": env("PG_PORT", "5432"),
+            "CONN_MAX_AGE": None,
+            "OPTIONS": {"sslmode": env("PG_SSL_MODE")},
+        }
+    }
 
 
 REST_FRAMEWORK = {
