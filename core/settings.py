f5c261098436d38f6098b7ca132fd53962f80283
Nathnael Yirga <Nathnaelyirga@gmail.com>
2025-05-22 03:23:19 +0300

    settings and pip list

--- core/settings.py
+++ core/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": [],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
@@ -181,7 +181,7 @@ AUTH_USER_MODEL = "authentication.CustomUser"
 SIMPLE_JWT = {
     # "AUTH_HEADER_TYPES": ("JWT",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_HEADER_TYPES": ("Bearer",),
     # "AUTH_USER_MODEL": "authentication.User",
 }
